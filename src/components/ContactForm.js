65ef44062a8fea405141a6991ea7311115ed22ae
Ayk <brutalmind99@gmail.com>
2023-08-17 02:50:55 +0800

    add useForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,11 +2,19 @@ import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import emailjs from "@emailjs/browser";
 import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { FaExclamationTriangle } from "react-icons/fa";
+// import { calcLength } from "framer-motion";
 
 export const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // console.log(errors);
   const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const sendEmail = (data) => {
     emailjs
       .sendForm(
         process.env.REACT_APP_EMAILJS_SERVICE_ID,
@@ -18,7 +26,8 @@ export const ContactForm = () => {
         (result) => {
           console.log(result.text);
           console.log("message sent");
-          e.target.reset();
+          alert("Thank you I will get back to you as soon as possible ! (:");
+          form.current.reset();
         },
         (error) => {
           console.log(error.text);
@@ -38,7 +47,10 @@ export const ContactForm = () => {
           action=""
           className="w-[80%] h-full"
           ref={form}
-          onSubmit={sendEmail}
+          onSubmit={handleSubmit(sendEmail)}
+          // onSubmit={handleSubmit((data) => {
+          //   console.log(data);
+          // })}
         >
           <div className="w-full flex flex-col mt-5">
             <label htmlFor="fullname" className="text-[25px] pb-1">
@@ -50,7 +62,16 @@ export const ContactForm = () => {
               name="user_fullname"
               className="p-[0.5em] rounded-lg text-black font-bold text-[23px]"
               placeholder="Enter your full name"
+              {...register("user_fullname", {
+                required: "Your Name is Required",
+              })}
             />
+            {errors.user_fullname && (
+              <span className="text-red-600 font-bold flex flex-row items-center pt-1 pl-1">
+                <FaExclamationTriangle />{" "}
+                <p className="pl-1">{errors.user_fullname?.message}</p>
+              </span>
+            )}
           </div>
           <div className="w-full flex flex-col mt-5">
             <label htmlFor="email" className="text-[25px] pb-1">
@@ -62,7 +83,20 @@ export const ContactForm = () => {
               name="user_email"
               className="p-[0.5em] rounded-lg text-black font-bold text-[23px]"
               placeholder="Enter your email"
+              {...register("user_email", {
+                required: "Enter a valid Email",
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                },
+              })}
             />
+            {errors.user_email && (
+              <span className="text-red-600 font-bold flex flex-row items-center pt-1 pl-1">
+                <FaExclamationTriangle />{" "}
+                <p className="pl-1">{errors.user_email?.message}</p>
+              </span>
+            )}
           </div>
           <div className="w-full flex flex-col mt-5">
             <label htmlFor="message" className="text-[25px] pb-1">
@@ -73,7 +107,14 @@ export const ContactForm = () => {
               name="message"
               className="p-[0.5em] rounded-lg text-black font-bold text-[23px]"
               placeholder="Enter your message..."
+              {...register("message", { required: "Message is required" })}
             ></textarea>
+            {errors.message && (
+              <span className="text-red-600 font-bold flex flex-row items-center pt-1 pl-1">
+                <FaExclamationTriangle />{" "}
+                <p className="pl-1">{errors.message?.message}</p>
+              </span>
+            )}
           </div>
           <div className="w-full flex justify-center items-center flex-col mt-5">
             <button
