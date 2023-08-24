64a18c9cc12b26d299aee447696d262a0035e380
Ayk <brutalmind99@gmail.com>
2023-08-24 16:22:32 +0800

    fix grammar

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -31,9 +31,9 @@ const introduction = {
   text: [
     "Hello there, I'm so happy you are here! (:",
 
-    "I'm 24 year's old, highly motivated Full-Stack Web Developer with ability to learn and collaborate in rapidly changing environments and compositions.",
+    "I'm 24 year's old, highly motivated Full-Stack Web Developer with the ability to learn and collaborate in rapidly changing environments and compositions.",
 
-    "Outside of my regular activities, I'm into music whenever I have some free time I play drums and enjoy skateboarding as well.",
+    "Outside of my regular activities, I'm into music and playing drums. In my free time I enjoy skateboarding as well.",
 
     "I'm seeking for an opportunity to grow my Web Development skills in an innovative environment. Your visit to my portfolio is deeply appreciated !",
   ],
